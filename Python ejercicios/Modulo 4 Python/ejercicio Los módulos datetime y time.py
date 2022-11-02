49060526e3774badefa3402699c13bb2bf468d6a
Ginna2-Pao90 <101013471+Ginna2-Pao90@users.noreply.github.com>
2022-11-02 18:14:24 -0500

    Delete Python ejercicios directory

--- Python ejercicios/Modulo 4 Python/ejercicio Los módulos datetime y time.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-dato = datetime(2022,11,2,17,50)
-print(dato.strftime("%Y/%m/%d %H:%M:%S"))
-print(dato.strftime("%y/%B/%d %H:%M:%S %p"))
-print(dato.strftime("%a/%Y/%b %d"))
-print(dato.strftime("%A/%Y/%B %d"))
-print(dato.strftime("Dia de la semana: %w"))
-print(dato.strftime("Dia del año: %j"))
-print(dato.strftime("Número de semana en el año: %W"))
